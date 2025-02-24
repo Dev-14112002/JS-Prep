dbe8d7afb2786a60f35211445c7c09256017516d
Dev-14112002 <gulatisaksham2002@gmail.com>
2025-02-25 00:27:38 +0530

    Updated files

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,6 +1,6 @@
 const accountId = 144;
-let accountEmail = "gulatisaksham1111@gmail.com";
-var accountPassword = "12345";
+// let accountEmail = "gulatisaksham1111@gmail.com";
+// var accountPassword = "12345";
 accountCity = "Delhi";
 
 // accountId = 2;

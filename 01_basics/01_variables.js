d32084885dc43441f2b4d74b4f33c72c4a871b95
Dev-14112002 <gulatisaksham2002@gmail.com>
2025-02-25 00:02:19 +0530

    Made changes and var

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,7 +2,25 @@ const accountId = 144;
 let accountEmail = "gulatisaksham1111@gmail.com";
 var accountPassword = "12345";
 accountCity = "Delhi";
+let accountState;
 
-accountId = 2;
+// accountId = 2;
+
+accountEmail = "sg@sg.com";
+accountPassword = "231231312";
+accountCity = "Noida";
 
 console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope & functional scope
+*/
+
+console.table([
+  accountId,
+  accountEmail,
+  accountPassword,
+  accountCity,
+  accountState,
+]);
